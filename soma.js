8a0b9737f1a1fcac7ccae2d842cecb57d4c0265f
YayaWeslla <yws2@aluno.ifal.edu.br>
2025-08-27 11:58:01 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,5 @@
+function soma(a, b) {
+    return a + b;
+}
+var a = 2;
+var resultado = soma(a, 3);
---
+++ soma.ts
@@ -0,0 +1,6 @@
+function soma(a: number, b: number): number{
+    return a + b;
+}
+
+const a = 2;
+const resultado = soma(a,3);
