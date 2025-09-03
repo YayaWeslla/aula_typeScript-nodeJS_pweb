815d11bda18c9b5713a1027b80b2cb5124bfe52f
YayaWeslla <yws2@aluno.ifal.edu.br>
2025-09-03 11:55:19 -0300

    Add files via upload

--- interface.ts
+++ interface.ts
@@ -1,12 +1,47 @@
-interface IUser {
-    id: number;
-    name: string;
-    password: string;
-    email?: string; //"?" diz que o atributo é opcional
+interface ICarro {
+    marca: string;
+    modelo: string;
+    ano: number;
+
+    exibirDetalhes(): void;
+    acelerar(velocidade: number): void;
+    frear(): void;
 }
-const usuario: IUser = {
-    id: 1,
-    name: "Alice",
-    password: "securepassword123",
-    email: "alice@gmail.com"
+
+class CarroAGasolina implements ICarro {
+    marca: string;
+    modelo: string;
+    ano: number;
+    private velocidade: number;
+
+    constructor(marca: string, modelo: string, ano: number){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.velocidade = 0;
+    }
+
+    exibirDetalhes(): void {
+        console.log(`Dados do veículo a gasolina`);
+        console.log(`Marca: ${this.marca}`);
+        console.log(`Modelo: ${this.modelo}`);
+        console.log(`Ano: ${this.ano}`)
+    }
+    acelerar(velocidade: number): void {
+        this.velocidade += velocidade;
+        //+= primeiro incrementa, depois usa o valor
+        //=+ usa o valor, depois incrementa
+        console.log(`Acelerando a ${this.velocidade} km/h`)
+    }
+    frear(): void {
+        this.velocidade = 0;
+        console.log(`O carro parou.`)
+    }
+
 }
+
+const carroGas = new CarroAGasolina("Wolkswagen", "Polo", 2018);
+carroGas.exibirDetalhes();
+carroGas.acelerar(50);
+carroGas.acelerar(30);
+carroGas.frear();
